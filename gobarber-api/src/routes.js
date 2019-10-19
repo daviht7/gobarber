d6d026d6077e9f1eac180c4d0255c81d76d88dc9
Davi Holanda <daviht7@gmail.com>
2019-10-19 20:14:15 -0300

    criação do loader model

--- gobarber-api/src/routes.js
+++ gobarber-api/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "heello" });
+routes.get("/users", async (req, res) => {
+  const user = await User.create({
+    name: "carlos almeida",
+    email: "kameta@gmail.com",
+    password_hash: "13213223",
+    provider: false
+  });
+  return res.json(user);
 });
 
 export default routes;
