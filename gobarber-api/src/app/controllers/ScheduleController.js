56daf02b8e03b0c86b3c252686abd6db9d8e6bff
Davi Holanda <daviht7@gmail.com>
2019-12-01 19:45:03 -0300

    ajustes na API

--- gobarber-api/src/app/controllers/ScheduleController.js
+++ gobarber-api/src/app/controllers/ScheduleController.js
@@ -1,22 +1,20 @@
-import Appointment from '../models/Appointment'
-import User from '../models/User'
+import Appointment from "../models/Appointment";
+import User from "../models/User";
 
-import { Op } from 'sequelize'
-import { startOfDay, endOfDay, parseISO } from 'date-fns'
+import { Op } from "sequelize";
+import { startOfDay, endOfDay, parseISO } from "date-fns";
 
 class ScheduleController {
-
   async index(req, res) {
-
     const checkUserProvider = await User.findOne({
       where: {
         id: req.userId,
         provider: true
       }
-    })
+    });
 
     if (!checkUserProvider) {
-      return res.status(401).json({ error: "user is not a provider." })
+      return res.status(401).json({ error: "user is not a provider." });
     }
 
     const { date } = req.query;
@@ -29,12 +27,12 @@ class ScheduleController {
           [Op.between]: [startOfDay(dateParsed), endOfDay(dateParsed)]
         }
       },
+      include: [{ model: User, as: "user", attributes: ["name"] }],
       order: ["date"]
-    })
+    });
 
-    return res.json(appointments)
+    return res.json(appointments);
   }
-
 }
 
 export default new ScheduleController();
