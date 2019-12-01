64a396aa3ee9192158bd24172f68921544b331dc
Davi Holanda <daviht7@gmail.com>
2019-12-01 14:29:42 -0300

    tratamento de erros com sentry

--- gobarber-api/src/app.js
+++ gobarber-api/src/app.js
@@ -1,18 +1,26 @@
 import express from "express";
+import "express-async-errors";
 import routes from "./routes";
 import "./database";
 import cors from "cors";
 import path from "path";
+import * as Sentry from "@sentry/node";
+import sentryConfig from "./config/sentry";
+import Youch from "youch";
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(
       cors({
         /*origin:"https//"*/
@@ -27,6 +35,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ gobarber-api/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: "https://c0498260a2654e52a56f75ba146ae346@sentry.io/1840578"
+};
