6bef0b733034b47f95f05db6c64ba9efed59ade4
Davi Holanda <daviht7@gmail.com>
2019-12-15 17:18:28 -0300

    abstraindo validação de criação de agendamento

--- gobarber-api/src/app/middlewares/auth.js
+++ gobarber-api/src/app/middlewares/auth.js
@@ -18,9 +18,6 @@ export default async (req, res, next) => {
 
     return next();
   } catch (err) {
-    console.log(err);
     return res.status(401).json({ error: "token invalid" });
   }
-
-  return next();
 };
